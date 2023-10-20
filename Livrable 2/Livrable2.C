1e48b3378d70bf3940f76eb6d327828e0cda2849
ntino67 <113057912+ntino67@users.noreply.github.com>
2023-10-20 14:35:12 +0200

    Nouveau trucs

--- Livrable 2/Livrable2.C
+++ Livrable 2/Livrable2.C
@@ -1,14 +1,14 @@
-#define LEDPIN 7
-#define PUSHPIN 2
+#define LEDPIN 7  // Définition du numéro de la broche de la LED
+#define PUSHPIN 2 // Définition du numéro de la broche du bouton
 
-int randNumber = 0;
-volatile bool data = false;
-unsigned long int start;
-unsigned long int stop;
+int randNumber = 0;         // Variable pour stocker le nombre aléatoire
+volatile bool data = false; // Variable pour stocker l'état du bouton
+unsigned long int start;    // Variable pour stocker le temps de début
+unsigned long int stop;     // Variable pour stocker le temps de fin
 
 void setup()
 {
-    Serial.begin(9600);
+    Serial.begin(9600);        // Initialisation port série
     randomSeed(analogRead(0)); // Initialisation générateur de nombres aléatoires
     pinMode(LEDPIN, OUTPUT);   // Initialisation LED
     pinMode(PUSHPIN, INPUT);   // Initialisation bouton
@@ -17,42 +17,44 @@ void setup()
 
 void loop()
 {
-    acquisition();
+    acquisition(); // Je simule l'acquisition de données
 }
 
-void acquisition() // Je simule l'acquisition de données avec cette fonction
-{
+void acquisition()
+{ // Je simule l'acquisition de données avec cette fonction
+
     randNumber = random(3, 7); // Je tire un nombre au hasard entre 3 et 6
     delay(randNumber * 1000);  // delai du nombre de secondes simulant l'acquisition de données
 }
 
 void basculer()
-{
-    data = !data;
+{ // Fonction appelée lors de l'interruption
+
+    data = !data; // Je bascule la valeur de data
     if (data)
-    {
-        allumer();
-        start = millis();
+    {                     // Si data est vrai
+        allumer();        // J'allume la LED
+        start = millis(); // Je récupère le temps en millisecondes
     }
     else
-    {
-        stop = millis();
-        eteindre();
-        Serial.print("temps de pressage du bouton : ");
-        Serial.println(stop - start);
+    {                                                   // Si data est faux
+        stop = millis();                                // Je récupère le temps en millisecondes
+        eteindre();                                     // J'éteins la LED
+        Serial.print("temps de pressage du bouton : "); // J'affiche le temps de pressage du bouton
+        Serial.println(stop - start);                   // J'affiche le temps de pressage du bouton
     }
 }
 void interruption_init(void)
-{
-    attachInterrupt(digitalPinToInterrupt(PUSHPIN), basculer, CHANGE);
+{                                                                      // Fonction d'initialisation de l'interruption
+    attachInterrupt(digitalPinToInterrupt(PUSHPIN), basculer, CHANGE); // J'attache l'interruption au bouton
 }
 
 void allumer()
-{
+{ // Fonction d'allumage de la LED
     digitalWrite(LEDPIN, HIGH);
 }
 
 void eteindre()
-{
+{ // Fonction d'extinction de la LED
     digitalWrite(LEDPIN, LOW);
 }
